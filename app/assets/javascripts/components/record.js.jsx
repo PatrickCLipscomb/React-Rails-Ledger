12cdfdd8af722f1f472d6ddb6f565d61c372af5c
patrick lipscomb <patrick74lipscomb@gmail.com>
2016-10-22 18:17:29 -0700

    it will fix the bugs that were occuring

--- app/assets/javascripts/components/record.js.jsx
+++ app/assets/javascripts/components/record.js.jsx
@@ -1,86 +1,93 @@
+'use strict';
 
+class Record extends BaseComponent {
+    constructor(props){
+        super(props);
+        this._bind('handleDelete', 'handleToggle', 'recordRow', 'recordForm', 'handleEdit');
+        this.state = {
+            edit: false
+        };
+    }
 
-class Record extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      editable: false
+    handleDelete(event) {
+        var id = "records/" + this.props.record.id;
+        event.preventDefault();
+        $.ajax({
+            method: 'DELETE',
+            url: id,
+            dataType: 'JSON',
+            success: ( () => {
+                this.props.handleDeleteRecord(this.props.record);
+            })
+        });
     }
-  }
-  render() {
-    return(
-      this.state.editable ? this.recordForm() : this.recordRow()
-    )
-  }
-  recordRow() {
-    return(
-      <tr>
-        <td>
-          {this.props.record.date}
-        </td>
-        <td>
-          {this.props.record.title}
-        </td>
-        <td>
-          {amountFormat(this.props.record.amount)}
-        </td>
-        <td>
-          <a className="btn btn-default" onClick={this.handleToggle}>Edit</a>
-          <a className="btn btn-danger" onClick={this.handleDelete.bind(this)}>Delete</a>
-        </td>
-      </tr>
-    )
-  }
-  recordForm() {
-    return(
-      <tr>
-        <td>
-          <input className="form-control" type="text" defaultValue={this.props.date} ref="date" />
-        </td>
-        <td>
-          <input className="form-control" type="text" defaultValue={this.props.title} ref="title" />
-        </td>
-        <td>
-          <input className="form-control" type="text" defaultValue={this.props.amount} ref="amount" />
-        </td>
-        <td>
-          <a className="btn btn-default" onClick={this.handleEdit.bind(this)} >Update</a>
-          <a className="btn btn-danger" onClick={this.handleToggle} >Cancel</a>
-        </td>
-      </tr>
-    )
-  }
-  handleEdit(event) {
-    event.preventDefault()
-    var newData = {
-      date: React.findDOMNode(this.refs.date).value,
-      title: React.findDOMNode(this.refs.title).value,
-      amount: React.findDOMNode(this.refs.amount).value
+
+    handleEdit(event) {
+        event.preventDefault();
+        var id = "records/" + this.props.record.id;
+        var data = {
+            title: ReactDOM.findDOMNode(this.refs.title).value,
+            date: ReactDOM.findDOMNode(this.refs.date).value,
+            amount: ReactDOM.findDOMNode(this.refs.amount).value
+        };
+        $.ajax({
+            method: 'PUT',
+            url: id,
+            dataType: 'JSON',
+            data: { record: data },
+            success: ( (data) => {
+                this.setState({ edit: false });
+                this.props.handleEditRecord(this.props.record, data);
+            })
+        });
     }
-    $.ajax({
-      method: 'PUT',
-      url: "/records/" + this.props.record.id,
-      dataType: 'JSON',
-      data: { record: newData },
-      success: ( (data) => {
-        this.setState({editable: false})
-        this.props.onEdit(this.props.record, data)
-      })
-    })
-  }
-  handelToggle(event) {
-    event.preventDefault()
-    this.setState({editable: !this.state.editable})
-  }
-  handleDelete(event) {
-    event.preventDefault()
-    $.ajax({
-      method: 'DELETE',
-      url: "/records/" + this.props.record.id,
-      dataType: 'JSON',
-      success: ( () => {
-        this.props.onDelete(this.props.record)
-      })
-    })
-  }
+
+    handleToggle(event) {
+        event.preventDefault();
+        this.setState({ edit: !this.state.edit });
+    }
+
+    recordRow() {
+        return (
+            <tr>
+                <td>{this.props.record.date}</td>
+                <td>{this.props.record.title}</td>
+                <td>{amountFormat(this.props.record.amount)}</td>
+                <td>
+                    <a className="btn btn-default" onClick={this.handleToggle}>Edit</a>
+                    <a className="btn btn-danger" onClick={this.handleDelete}>Delete</a>
+                </td>
+            </tr>
+        );
+    }
+
+    recordForm() {
+        return (
+            <tr>
+                <td>
+                    <input className="form-control" type="text" defaultValue={this.props.record.date} ref="date"/>
+                </td>
+                <td>
+                    <input className="form-control" type="text" defaultValue={this.props.record.title} ref="title"/>
+                </td>
+                <td>
+                    <input className="form-control" type="number" defaultValue={this.props.record.amount} ref="amount"/>
+                </td>
+                <td>
+                    <a className="btn btn-default" onClick={this.handleEdit}>Update</a>
+                    <a className="btn btn-danger" onClick={this.handleToggle}>Cancel</a>
+                </td>
+            </tr>
+        );
+    }
+
+    render() {
+        return this.state.edit ? this.recordForm() : this.recordRow();
+    }
 }
+
+Record.propTypes = {
+    record: React.PropTypes.object.isRequired,
+    handleDeleteRecord: React.PropTypes.func.isRequired,
+    handleEditRecord: React.PropTypes.func.isRequired
+};
--- app/assets/javascripts/components/records.js.jsx
+++ app/assets/javascripts/components/records.js.jsx
@@ -1,79 +1,96 @@
+/**
+ * Created by alexdaube on 15-07-02.
+ */
 
+'use strict';
 
-class Records extends React.Component {
-  constructor(props) {
-    super()
-    this.state = {
-      records: props.data
+class Records extends BaseComponent {
+    constructor(props) {
+        super();
+        this._bind('addRecord', 'credits', 'debits', 'balance', 'deleteRecord', 'handleEditRecord');
+        this.state = {
+            records: props.data
+        };
     }
-  }
-  render () {
-    const records = this.state.records.map( (record) =>
-      <Record key={record.id} record={record} onEdit={this.editRecord.bind(this)} onDelete={this.deleteRecord.bind(this)}/>
-  )
-    return(
-      <div className="records">
-        <h2 className="title">Records</h2>
-        <AmountBox type="success" amount={this.credits()} text="Credit" />
-        <AmountBox type="danger" amount={this.debits()} text="Debit" />
-        <AmountBox type="info" amount={this.balance()} text="Balance" />
-        <RecordForm handleNewRecord={this.addRecord.bind(this)} />
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Title</th>
-              <th>Amount</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {records}
-          </tbody>
-        </table>
-      </div>
-    )
-  }
-  editRecord(record, data) {
-    var index = this.state.records.slice().indexOf(record)
-    this.setState({records: this.state.records.splice(index, 1, data)})
-  }
-  addRecord(record) {
-    console.log(this.state.records)
-    var records = this.state.records.slice()
-    records.push(record)
-    this.setState({records: records})
-  }
-  deleteRecord(record) {
-    console.log(this.state.records)
-    var records = this.state.records.slice()
-    var index = records.indexOf(record)
-    this.setState({records: records.splice(index, 1)})
-  }
-  addRecord(record) {
-    var updatedRecords = this.state.records
-    updatedRecords.push(record)
-    this.setState({records: updatedRecords})
-  }
-  credits() {
-    return this.state.records
-      .filter((record) => {
-        return record.amount >= 0
-      })
-      .reduce((previous, current) => {
-        return previous + parseFloat(current.amount)
-      }, 0)
-  }
-  debits() {
-    return this.state.records
-      .filter((record) => {
-        return record.amount < 0
-      })
-      .reduce((previous, current) => {
-        return previous + parseFloat(current.amount)
-      }, 0)
-  }
-  balance() {
-    return this.credits() + this.debits()
-  }
+
+    addRecord(record) {
+
+        var records = React.addons.update(this.state.records, {$push: [record]});
+        this.setState({ records: records });
+    }
+
+    deleteRecord(record) {
+        var index = this.state.records.indexOf(record);
+        var records = React.addons.update(this.state.records, {$splice: [[index, 1]]});
+        this.setState({ records: records });
+    }
+
+    handleEditRecord(record, data) {
+        var index = this.state.records.indexOf(record);
+        var records = React.addons.update(this.state.records, {$splice: [[index, 1, data]]});
+        this.setState({ records: records} );
+    }
+
+    credits() {
+        return this.state.records
+            .filter( (record) =>{
+                return record.amount >= 0;
+            })
+            .reduce( (previous, current) => {
+                return previous + parseFloat(current.amount);
+            }, 0);
+    }
+
+    debits() {
+        return this.state.records
+            .filter( (record) => {
+                return record.amount < 0;
+            })
+            .reduce( (previous, current) => {
+                return previous + parseFloat(current.amount);
+            }, 0);
+    }
+
+    balance() {
+        return this.debits() + this.credits();
+    }
+
+    render() {
+        var records = this.state.records.map((record, index) => {
+            return <Record key={record.id} record={record}
+                           handleDeleteRecord={this.deleteRecord} handleEditRecord={this.handleEditRecord} />
+        });
+        return (
+            <div className="records">
+                <h2 className="title"> Records </h2>
+                <h5 className="title">Add a record</h5>
+                <RecordForm handleNewRecord={this.addRecord} />
+                <hr />
+                <div className="row">
+                  <AmountBox type="success" amount={this.credits()} text="Credit"/>
+                  <AmountBox type="danger" amount={this.debits()} text="Debit"/>
+                  <AmountBox type="info" amount={this.balance()} text="Balance"/>
+                </div>
+                <hr />
+                <h5 className="title">Ledger Items</h5>
+                <table className="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th> Date </th>
+                            <th> Title </th>
+                            <th> Amount </th>
+                            <th> Actions </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    {records}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
+
+Records.defaultProps = {
+    records: []
+};
